3eca36c8cd3787039a9b281b80bf2296f5f66693
David_da_Annin <Davidannin22@gmail.com>
2025-09-03 15:05:48 +0200

    function that takes an array and other arguments then removes the other arguments from that array.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(firstSet,...secondSet) {
+    
+    const newArray = [];
+    
+    firstSet.forEach((item) => {
+
+        if (!secondSet.includes(item)){
+            newArray.push(item);
+        }
+
+
+    });
+    return newArray;
+   
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
